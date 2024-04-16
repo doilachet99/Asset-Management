d6683040e5fdcf1bb32b59dd9750f81fa114a7b6
doilachet99 <55074980+doilachet99@users.noreply.github.com>
2024-04-16 21:52:45 +0700

    add source

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {          
+            return View( new Devices().GetDevices());
+        }
+
+
+        public ActionResult Index2()
+        {
+            return View( new Devices().GetDevices());
+        }
+
+    }
+}
---
+++ WebApplication1/Models/Devices.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+
+namespace WebApplication1.Models
+{
+    public class Devices
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+        public List<Devices> GetDevices()
+        {
+            List<Devices> devicesList = new List<Devices>();
+
+            devicesList.Add(new Devices()
+            {
+                Id = 1,
+                Name = "Test",
+                Description = "Test-1"
+            });
+
+            devicesList.Add(new Devices()
+            {
+                Id = 2,
+                Name = "Test2",
+                Description = "Test-2"
+            });
+            devicesList.Add(new Devices()
+            {
+                Id = 3,
+                Name = "Test3",
+                Description = "Test-3"
+            });
+            devicesList.Add(new Devices()
+            {
+                Id = 4,
+                Name = "Test4",
+                Description = "Test-4"
+            });
+            return devicesList;
+        }
+    }
+}
